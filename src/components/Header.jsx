2da049b5d5ebef2f00f854f826e2a5246e4ed870
Aayush Singh Chauhan <1211aayushh@gmail.com>
2026-09-18 16:35:03 +0530

    feat: add dropdown for different models

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
 
     return(
         <div className="flex items-center m-4">
-            <img src="src/assets/image.png" alt="" className="w-12 h-12"/>
+            <img src="https://imgs.search.brave.com/Ay1St93MenLC5FJmlz77Bgpv1gS6IOUMoRjnFusL57s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yYXcu/Z2l0aHVidXNlcmNv/bnRlbnQuY29tL2xv/YmVodWIvbG9iZS1p/Y29ucy9yZWZzL2hl/YWRzL21hc3Rlci9w/YWNrYWdlcy9zdGF0/aWMtcG5nL2xpZ2h0/L2dlbWluaS1jb2xv/ci5wbmc" alt="" className="w-12 h-12"/>
             <p className="text-white ml-2">Gemini</p>
         </div>
     )
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -21,6 +21,11 @@ function handleKeyDown (e) {
     return(
         <div className="bg-zinc-800 p-3 rounded-full  w-full max-w-2xl h-15 flex justify-between items-center">
             <input type="text" placeholder="Ask Gemini" className= "outline-none m-2 text-white  w-full h-full" value={question} onChange={(event)=> {setQuestion(event.target.value)}} onKeyDown={handleKeyDown}/>
+            <select className="text-white bg-zinc-800 rounded-full outline-none">
+                <option value="3.5">3.5 Flash Lite</option>
+                <option value="3.6">3.6 Flash</option>
+                <option value="3.1">3.1 pro</option>
+            </select>
             <button className="text-white m-2" onClick={inputHandler}>ASK</button>
         </div>
     )
